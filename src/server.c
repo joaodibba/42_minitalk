1761af8fffb4e354e9ef49893bea2e61ff8bd6e7
rajh-phuyal <rajhphuyal44@gmail.com>
2023-04-18 13:36:26 +0100

    server update

--- src/server.c
+++ src/server.c
@@ -12,25 +12,30 @@ void	send_back_responce(void)
 void	decode_and_print_char(int sig)
 {
 	if (sig == SIGUSR1)
-		g_process.current_char |= (1 << g_process.bit_count);
+	{
+		if (g_process.is_pid)
+			g_process.client_pid |= (1 << g_process.bit_count);
+		else
+			g_process.current_char |= (1 << g_process.bit_count);
+	}
 	g_process.bit_count++;
 	ft_printf("SIGUSR1: %i | bit_count: %i\n", (sig == SIGUSR1), g_process.bit_count);
 	if (g_process.bit_count == 8)
 	{
 		if (g_process.is_pid)
 		{
-			g_process.client_pid = (int)g_process.current_char;
 			g_process.is_pid = 0;
 			ft_printf("Client pid: %i", g_process.client_pid);
 		}
 		else
 		{
-			ft_printf("current char: %c\n", g_process.current_char);
-			g_process.bit_count = 0;
 			if (!g_process.current_char)
 				send_back_responce();
+			else
+				ft_printf("current char: %c\n", g_process.current_char);
 			g_process.current_char = 0;
 		}
+		g_process.bit_count = 0;
 	}
 }
 
