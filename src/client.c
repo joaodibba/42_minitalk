3c30785a4dce13e5cb5d06610306d04de7c93237
joaodibba <joaodibba@joaodibba.com>
2023-04-19 15:50:32 +0100

    added explaining comments

--- src/client.c
+++ src/client.c
@@ -1,11 +1,15 @@
 
 #include "../include/minitalk.h"
 
+/* The server_response function is called whenever the client receives a response signal from the server.
+ It simply prints a message to indicate that the response has been received.*/
 void	server_response(int sig)
 {
 	ft_printf("message received. sig: %i\n", sig);
 }
 
+/*The encode_and_send_char function encodes a single character into 8 bits
+ and sends the bits to the server process by sending SIGUSR1 or SIGUSR2 signals for each bit.*/
 void	encode_and_send_char(pid_t server_pid, int c)
 {
 	int	i;
@@ -23,6 +27,9 @@ void	encode_and_send_char(pid_t server_pid, int c)
 	}
 }
 
+/*The send_message function encodes and sends each character in the input message
+ to the server using the encode_and_send_char function.
+ It then waits for a response signal from the server using the pause function.*/
 void	send_message(pid_t server_pid, char *message)
 {
 	while (*message)
@@ -32,6 +39,10 @@ void	send_message(pid_t server_pid, char *message)
 	pause();
 }
 
+/*The main function for the client process first retrieves the client and server PIDs
+ from the command-line arguments. It then sets up the signal handler for SIGUSR1
+ and calls the encode_and_send_char function to encode and send the client PID to the server.
+ Finally, it calls the send_message function to send the input message to the server and waits for a response signal.*/
 int	main(int argc, char **argv)
 {
 	pid_t	server_pid;
--- src/server.c
+++ src/server.c
@@ -3,12 +3,24 @@
 
 t_bits	g_process;
 
+/*The send_back_response function is responsible for sending a signal
+ to the client that the message has been received successfully.
+  It sends the SIGUSR1 signal to the client process using the kill function.*/
+
 void	send_back_response(void)
 {
 	kill(g_process.client_pid, SIGUSR1);
 	ft_printf("OK\n");
 }
 
+/* The decode_and_print_char function is called whenever the server receives
+ a signal from the client. It decodes the signal and prints the result.
+ If the signal is SIGUSR1, it means that a bit is set to 1, and the function
+ sets the corresponding bit in either the client PID or the current character being decoded,
+ depending on the value of the is_pid flag in the g_process struct. If the signal is SIGUSR2,
+ it means that a bit is set to 0, and the function does nothing except increment the bit count.
+ Once 8 bits have been received, the function either prints the client PID or the decoded character,
+ depending on the value of the is_pid flag, and resets the bit count and current character.*/
 void	decode_and_print_char(int sig)
 {
 	if (sig == SIGUSR1)
@@ -39,6 +51,9 @@ void	decode_and_print_char(int sig)
 	}
 }
 
+/* The main function initializes the g_process struct and sets up
+ the signal handlers for SIGUSR1 and SIGUSR2.
+ It then enters an infinite loop and waits for signals to arrive.*/
 int	main(void)
 {
 	signal(SIGUSR1, decode_and_print_char);
