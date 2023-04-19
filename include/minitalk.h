49aebd12b5bd777aa4cef5aea405562ed54d45b7
Joao Tomas Alves Costa Miranda <jalves-c@c1r3s2.42lisboa.com>
2023-04-19 15:29:35 +0100

    updated struct

--- include/minitalk.h
+++ include/minitalk.h
@@ -7,6 +7,13 @@
 # include <unistd.h>
 # include <signal.h>
 
+typedef struct s_bits
+{
+	int				is_pid;
+	int				bit_count;
+	int				client_pid;
+	unsigned char	current_char;
+}	t_bits;
 
 void	send_message(pid_t server_pid, char *message);
 void	encode_and_send_char(pid_t server_pid, int c);
