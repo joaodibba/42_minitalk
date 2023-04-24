1346686a79630dc52e9f1f3910f242da5708813c
joaodibba <joaomiranda.pessoal@gmail.com>
2023-04-24 01:58:48 +0100

    made some changes

--- include/minitalk.h
+++ include/minitalk.h
@@ -6,7 +6,7 @@
 /*   By: jalves-c < jalves-c@student.42lisboa.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 00:56:55 by jalves-c          #+#    #+#             */
-/*   Updated: 2023/04/24 00:57:00 by jalves-c         ###   ########.fr       */
+/*   Updated: 2023/04/24 01:52:25 by jalves-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 # define MINITALK_H
 
 # define SHORT_SLEEP_TIME 100
-# define LONG_SLEEP_TIME 500
+# define MEDIUM_SLEEP_TIME 500
+# define LONG_SLEEP_TIME 1000
 
 # include "../libft/libft.h"
 # include <unistd.h>
--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: jalves-c < jalves-c@student.42lisboa.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/23 20:53:54 by jalves-c          #+#    #+#             */
-/*   Updated: 2023/04/24 01:08:00 by jalves-c         ###   ########.fr       */
+/*   Updated: 2023/04/24 01:53:06 by jalves-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,8 @@ void	send_message(pid_t server_pid, const char *message)
 
 	if (ft_strlen(message) <= 100)
 		sleep_time = SHORT_SLEEP_TIME;
+	else if (ft_strlen(message) <= 1000)
+		sleep_time = MEDIUM_SLEEP_TIME;
 	else
 		sleep_time = LONG_SLEEP_TIME;
 	while (*message)
@@ -53,19 +55,22 @@ int	arguments_check(t_args *args, int argc, char **argv)
 		ft_printf("\033[0;31mError: ./client [PID] [MESSAGE]\n\033[0m");
 		return (1);
 	}
-	args->server_pid = ft_atoi(argv[1]);
-	if (kill(args->server_pid, 0) == -1)
+	if (kill(ft_atoi(argv[1]), 0) == -1)
 	{
-		ft_printf("\033[0;31mPID %d is Invalid\n\033[0m", args->server_pid);
+		ft_printf("\033[0;31mPID %d is Invalid\n\033[0m", ft_atoi(argv[1]));
 		return (1);
 	}
-	else if (!ft_isprint(*argv[2]) || !ft_isdigit(*argv[1]))
+	else if (!ft_isdigit(*argv[1]))
 	{
 		ft_printf("\033[0;31mInvalid CLI arguments\n\033[0m");
 		return (1);
 	}
 	else
+	{
+		args->server_pid = ft_atoi(argv[1]);
+		args->message = argv[2];
 		return (0);
+	}
 }
 
 int	main(int argc, char **argv)
@@ -75,6 +80,6 @@ int	main(int argc, char **argv)
 	signal(SIGUSR1, positive_signal_received);
 	if (arguments_check(&args, argc, argv))
 		return (0);
-	send_message(args.server_pid, argv[2]);
+	send_message(args.server_pid, args.message);
 	return (0);
 }
